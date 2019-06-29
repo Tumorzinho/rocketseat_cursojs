39aa5f4f3bf112d26ab6e09d784ec220af9e38e7
Tumorzinho <Tumorzinho@github.com>
2019-06-28 23:48:24 -0300

    Criado a call da função para realizá-la

--- mod4/ex1/main.js
+++ mod4/ex1/main.js
@@ -5,3 +5,11 @@ function checaIdade(idade) {
       }, 2000);
     });
   }
+
+  checaIdade(15)
+  .then(function() {
+    console.log("Maior que 18");
+  })
+  .catch(function() {
+    console.log("Menor que 18");
+  });
