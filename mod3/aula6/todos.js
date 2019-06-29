99882c626a1277aa3145bfda950bceb0d6da94f9
Tumorzinho <Tumorzinho@github.com>
2019-06-28 23:31:54 -0300

    Transformada a vaŕiável todos em uma recuperação de dados do armazenamento local

--- mod3/aula6/todos.js
+++ mod3/aula6/todos.js
@@ -2,12 +2,7 @@ var listElement = document.querySelector("#app ul");
 var inputElement = document.querySelector("#app input");
 var buttonElement = document.querySelector("#app button");
 
-var todos = [
-    "Fazer café",
-    "Estudar Javascript",
-    "Acessar comunidade da Rocketseat"
-];
-
+var todos = JSON.parse(localStorage.getItem("list_todos"));
 function renderTodos() {
         listElement.innerHTML = "";
         for (todo of todos) {
